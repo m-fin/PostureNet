302f1427660c2de818c41f4a7c63f3fb28612ccb
Matthew Finley <mattjf67@gmail.com>
2019-10-12 21:57:15 -0400

    Chart

--- public/sketch.js
+++ public/sketch.js
@@ -113,6 +113,7 @@ function postureChart() {
   let percentageBadPosture = posture.timeBadPosture / totalPostureTime;
 
   //Chart
+  new Chart(document.getElementById("myChart"), { "type": "doughnut", "data": { "labels": ["Red", "Blue", "Yellow"], "datasets": [{ "label": "My First Dataset", "data": [300, 50, 100], "backgroundColor": ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"] }] } });
 
   // var chart = new CanvasJS.Chart("chartContainer", {
   //   data: [{
